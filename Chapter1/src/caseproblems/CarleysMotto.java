c4b6d81108a4204f411d7816ab976f4ea857bbcd
Nicholas02 <nickantle5@gmail.com>
2017-03-28 13:10:04 -0500

    update

---
+++ Chapter1/src/caseproblems/CarleysMotto.java
@@ -0,0 +1,14 @@
+package caseproblems;
+
+public class CarleysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*************************************************");
+		System.out.println("*Carly's Makes the food that makes it the party!*");
+		System.out.println("*************************************************");
+		System.out.println("*       Carly's Makes it fun in the sun!        *");
+		System.out.println("*************************************************");
+	}
+
+}
