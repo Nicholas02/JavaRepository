2fbac730a3cad3302b0c8a4dc9934b03b3b8fa8d
Nicholas02 <nickantle5@gmail.com>
2017-03-29 11:48:42 -0500

    update

---
+++ Chapter1/src/caseproblems/firstdialog.java
@@ -0,0 +1,12 @@
+package caseproblems;
+
+import javax.swing.JOptionPane;
+
+public class firstdialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	JOptionPane.showMessageDialog(null,  "First java Dialog.");
+	}
+
+}
---
+++ Chapter1/src/caseproblems/helloworld.java
@@ -0,0 +1,12 @@
+package caseproblems;
+
+import javax.swing.JOptionPane;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,  "Hello World.");
+	}
+
+}
