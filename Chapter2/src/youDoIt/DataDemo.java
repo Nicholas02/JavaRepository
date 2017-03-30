a6d634022eb35f3a26dc8e721c15ce265cf29d2a
Nicholas02 <nickantle5@gmail.com>
2017-03-30 12:49:12 -0500

    update

---
+++ Chapter2/src/youDoIt/DataDemo.java
@@ -0,0 +1,14 @@
+package youDoIt;
+
+public class DataDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Int aWholeNumber = 315;
+int STATES_IN_US = 50;
+System.out.println("The Number is " +  aWholeNumber);
+
+System.out.println("The number of states is " + STATES_IN_US );
+	}
+
+}
