016e592910dc2699d320d32f71195d6634ffa6e1
Nicholas02 <nickantle5@gmail.com>
2017-04-04 11:49:22 -0500

    update

--- Chapter1/src/exercises/Comments.java
+++ Chapter1/src/exercises/Comments.java
@@ -2,9 +2,22 @@ package exercises;
 
 public class Comments {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		System.out.println("Program comments are nonexecuting statments you add to a file for the purpose of documentation.");
+		
+	/**	System.out.println("Program comments are nonexecuting statments you add to a file for the purpose of documentation.");
+		**/
+		
+		/**System.out.println("Program comments are nonexecuting statments you add to a file for the purpose of documentation.");
+		**/
+		
+		/**	System.out.println("Program comments are nonexecuting statments you add to a file for the purpose of documentation.");
+		**/
+		
+		
 	}
 
 }
